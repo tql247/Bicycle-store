69c2279fd59bc2da34adf6121b863195295b64b7
tql247 <toitenlalinh9xpro@gmail.com>
2020-11-19 16:10:37 +0700

    Updates

---
+++ precache-manifest.91144406e5ddbe89ca6212273cea8076.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db20601a11bfa4f3730a251bc4f812e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "862c3b3b11dcc20cd54e",
+    "url": "/static/css/2.2d5861c9.chunk.css"
+  },
+  {
+    "revision": "422889080da0397bd500",
+    "url": "/static/css/main.b8d90e83.chunk.css"
+  },
+  {
+    "revision": "862c3b3b11dcc20cd54e",
+    "url": "/static/js/2.12cf5d1f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.12cf5d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422889080da0397bd500",
+    "url": "/static/js/main.350b9b9e.chunk.js"
+  },
+  {
+    "revision": "a81858db3a4d79c52ef7",
+    "url": "/static/js/runtime-main.001240a5.js"
+  }
+]);
