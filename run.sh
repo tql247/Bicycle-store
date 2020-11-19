506e79366ca0a6659a5b94eeb2eab987b148cbbf
tql247 <toitenlalinh9xpro@gmail.com>
2020-11-19 16:58:07 +0700

    test

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-cd web && npm run start & \
+cd web && npm run product & \
 cd server && uvicorn main:app --host 127.0.0.1 --port 9000
--- web/src/index.js
+++ web/src/index.js
@@ -4,4 +4,5 @@ import App from "./component/App";
 import "./index.css";
 import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
+
 ReactDOM.render(<App />, document.getElementById("root"));
